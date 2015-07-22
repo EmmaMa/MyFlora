d5774807880f3c9e91a246c9d5b9d5c4798f75d9
Gavan Kwan <gavan@Gavans-MacBook-Pro.local>
2015-07-22 11:36:45 -0700

    Add flower schema

--- both/collection/flower.js
+++ both/collection/flower.js
@@ -1,1 +1,19 @@
 Flowers = new Mongo.Collection("flowers");
+
+Flowers.attachSchema(new SimpleSchema({
+  title: {
+    type: String,
+    max: 200,
+    autoform: {
+      'label-type': 'stacked'
+    }
+  },
+  name: {
+    type: String,
+    max: 200,
+    autoform: {
+      'label-type': 'stacked'
+    }
+  },
+
+}));
--- server/seeds.js
+++ server/seeds.js
@@ -3,7 +3,8 @@ Meteor.startup(function () {
 if (Flowers.find({}).count() === 0) {
     Flowers.insert({
 		name: 'Roses',
-	    price: 1000,
+		id: 'roses',
+	    price: '$10',
 		desc: 'A beautiful bouquet of roses.',
 		purchaseLocation: 'http://roses.com',
 		company: 'Rose Company',
